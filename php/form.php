15e38c7e51993ae50a7ba59ea23ec032cfe6981c
yoooshi1105 <yoshitoakutohayato3@gmail.com>
2026-04-27 16:22:05 +0900

    man,womenに修正

--- php/form.php
+++ php/form.php
@@ -10,32 +10,33 @@
     <h1>フォーム入力</h1>
     <form action="confirm.php" method="post">
         <div class="form">
-        <label for="name">名前：</label>
-        <input type="text" id="name" name="name">
+            <label for="name">名前：</label>
+            <input type="text" id="name" name="name">
 
-        <label for="age">年齢：</label>
-        <input type="text" id="age" name="age">
+            <label for="age">年齢：</label>
+            <input type="text" id="age" name="age">
 
-        <label for="phone">電話番号：</label>
-        <input type="tel" id="phone" name="phone">
+            <label for="phone">電話番号：</label>
+            <input type="tel" id="phone" name="phone">
 
-        <label for="email">メールアドレス：</label>
-        <input type="email" id="email" name="email">
+            <label for="email">メールアドレス：</label>
+            <input type="email" id="email" name="email">
 
-        <label for="address">住所：</label>
-        <input type="text" id="address" name="address">
+            <label for="address">住所：</label>
+            <input type="text" id="address" name="address">
 
-        <label for="question">質問：</label>
-        <input type="text" id="question" name="question">
+            <label for="question">質問：</label>
+            <input type="text" id="question" name="question">
 
-        <label for="gender">性別：</label>
-        <select id="gender" name="gender">
-            <option value="">選択してください</option>
-            <option value="男性">男性</option>
-            <option value="女性">女性</option>
-        </select>
+            <label for="gender">性別：</label>
+            <select id="gender" name="gender">
+                <option value="">選択してください</option>
+                <option value="man">男性</option>
+                <option value="women">女性</option>
+            </select>
 
-         
+            <input type="submit" id="submit" name="submit">
+        </div>
     </form>
-        <input type="submit" id="submit" name="submit">
+       
 </body>
